2748a331bb9a0110c68f4a362c93093cfaedb267
didou0310 <104097523+didou0310@users.noreply.github.com>
2023-06-21 02:44:56 +0800

    Update home_page.dart

--- chat-app/lib/pages/home_page.dart
+++ chat-app/lib/pages/home_page.dart
@@ -1,41 +1,58 @@
+import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:chat_app/auth.dart';
-import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
   HomePage({Key? key}) : super(key: key);
-  
+
   final User? user = Auth().currentUser;
 
   Future<void> signOut() async {
     await Auth().signOut();
   }
+
   
   Widget _title() {
-    return const Text('Firebase Auth');
+    return const Text(
+      'Firebase Auth',
+      style: TextStyle(color: Colors.white),
+    );
   }
 
   Widget _userUid() {
     return Text(user?.email ?? 'User email');
   }
-
-  Widget _signOutButton() {
-    return ElevatedButton(
+  
+Widget _signOutButton() {
+  return Align(
+    alignment: Alignment.bottomCenter,
+    child: ElevatedButton(
       onPressed: signOut,
-      child: const Text('Sign Out'),
-      );
-  }
+      child: const Text(
+        'Sign Out',
+        style: TextStyle(color: Colors.white),
+      ),
+      style: ButtonStyle(
+        backgroundColor: MaterialStateProperty.all<Color>(Color(0xFF582841)),
+        foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
+      ),
+    ),
+  );
+}
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: _title(),
+        backgroundColor: Color(0xFF582841),
       ),
       body: Container(
         height: double.infinity,
         width: double.infinity,
         padding: const EdgeInsets.all(20),
+        color: Color(0xFFEFEFEF),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisAlignment: MainAxisAlignment.center,
@@ -44,8 +61,8 @@ class HomePage extends StatelessWidget {
             _signOutButton(),
           ],
         ),
-      )
-    ); 
+      ),
+    );
   }
 }
 
