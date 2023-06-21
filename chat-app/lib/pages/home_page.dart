eadbb3dc835cf3b0685f3e989f63a8abb4772eaf
didou0310 <104097523+didou0310@users.noreply.github.com>
2023-06-21 16:50:01 +0800

    Update home_page.dart

--- chat-app/lib/pages/home_page.dart
+++ chat-app/lib/pages/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:chat_app/auth.dart';
+import 'package:chat_app/pages/chatPage1.dart';
 
 class HomePage extends StatelessWidget {
   HomePage({Key? key}) : super(key: key);
@@ -11,7 +12,17 @@ class HomePage extends StatelessWidget {
     await Auth().signOut();
   }
 
-  
+  void navigateToChatPage(BuildContext context) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => ChatPage(
+          email: 'ahmadnorsafwan@gmail.com',
+        ),
+      ),
+    );
+  }
+
   Widget _title() {
     return const Text(
       'Firebase Auth',
@@ -22,24 +33,39 @@ class HomePage extends StatelessWidget {
   Widget _userUid() {
     return Text(user?.email ?? 'User email');
   }
-  
-Widget _signOutButton() {
-  return Align(
-    alignment: Alignment.bottomCenter,
-    child: ElevatedButton(
-      onPressed: signOut,
+
+  Widget _signOutButton() {
+    return Align(
+      alignment: Alignment.bottomCenter,
+      child: ElevatedButton(
+        onPressed: signOut,
+        child: const Text(
+          'Sign Out',
+          style: TextStyle(color: Colors.white),
+        ),
+        style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.all<Color>(Color(0xFF582841)),
+          foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
+        ),
+      ),
+    );
+  }
+
+  Widget _chatButton(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () {
+        navigateToChatPage(context);
+      },
       child: const Text(
-        'Sign Out',
+        'Chat with Ahmad',
         style: TextStyle(color: Colors.white),
       ),
       style: ButtonStyle(
         backgroundColor: MaterialStateProperty.all<Color>(Color(0xFF582841)),
         foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
       ),
-    ),
-  );
-}
-
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -58,6 +84,9 @@ Widget _signOutButton() {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             _userUid(),
+            SizedBox(height: 20),
+            _chatButton(context),
+            SizedBox(height: 100), // Add space of 100 pixels
             _signOutButton(),
           ],
         ),
